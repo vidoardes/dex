487629e979c55755a2cbac163ab4e4497fdfe422
David Rose <david.rose@brokertech.co.uk>
2018-07-31 18:24:17 +0100

    Added BrowserSync to gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,27 @@
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync');
+
+var reload = browserSync.reload;
+var exec = require('child_process').exec;
 
 function swallowError (error) {
-
-  // If you want details of the error in the console
   console.log(error.toString())
-
   this.emit('end')
 }
 
 gulp.task('sass', function () {
-    return gulp.src(['app/static/styles/*.scss', 'app/static/styles/**/*.scss'])
+    return gulp.src(['app/static/styles/main.scss'])
         .pipe(sass({outputStyle: 'compressed'}))
         .on('error', swallowError)
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('app/static/styles'))
+        .pipe(gulp.dest('app/static/'))
 });
 
 gulp.task('default', function () {
     gulp.start('sass');
+    browserSync({notify: false, proxy: "127.0.0.1:5003"});
     gulp.watch(['app/static/styles/*.scss', 'app/static/styles/**/*.scss'], ['sass']);
+    gulp.watch(['templates/**/*.*', 'app/static/styles/*.scss', 'app/static/styles/**/*.scss'], reload);
 });
