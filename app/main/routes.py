4d6a0c8dbafb026ae2eeb323d6523e7a4afbc874
David Rose <david@brokertech.co.uk>
2018-08-07 22:29:44 +0100

    Removed unused page

--- app/main/routes.py
+++ app/main/routes.py
@@ -17,25 +17,3 @@ def user(username):
     else:
         return render_template("main/user.html", user=user)
 
-
-@bp.route("/edit_profile/<username>", methods=["GET", "POST"])
-@login_required
-def edit_profile(username):
-    user = User.query.filter_by(username=username).first_or_404()
-
-    if current_user.username == user.username:
-        form = EditProfileForm(current_user.username)
-
-        if form.validate_on_submit():
-            user.set_password(form.password.data)
-            db.session.commit()
-            flash("Your changes have been saved.")
-            return redirect(url_for("edit_profile"))
-        elif request.method == "GET":
-            form.email.data = current_user.email
-
-        return render_template(
-            "main/edit_profile.html", title="Edit Profile", form=form
-        )
-    else:
-        return json.dumps({"success": False}), 403, {"ContentType": "application/json"}
