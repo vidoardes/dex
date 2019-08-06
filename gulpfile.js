6ba99d0d94f9288df9ddf57ccca0aaf4f9ce7f52
David Rose <david@brokertech.co.uk>
2019-08-06 17:35:26 +0100

    Updated gulpfile to newer style

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,111 @@
-const gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    rename = require("gulp-rename"),
-    browserSync = require("browser-sync"),
-    webpack = require("webpack-stream"),
-    zip = require("gulp-zip"),
-    clean = require("gulp-clean"),
-    runSequence = require("run-sequence")
+"use strict";
 
-let reload = browserSync.reload
-let exec = require("child_process").exec
+// Load plugins
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const browsersync = require("browser-sync");
+const webpack = require("webpack-stream");
+const zip = require("gulp-zip");
+const del = require("del");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
 
 function swallowError(error) {
-    console.log(error.toString())
+    console.log(error.toString());
     this.emit("end")
 }
 
-gulp.task("js", function () {
-    return gulp.src("app/static/scripts/main.js")
+// BrowserSync
+function browserSync(done) {
+    browsersync.init({
+        notify: false,
+        proxy: "127.0.0.1:5003",
+        port: 3000
+    });
+    done();
+}
+
+// BrowserSync reload
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
+
+// Compile CSS
+function css() {
+    return gulp
+        .src(["app/static/styles/main.scss"])
+        .pipe(sass({outputStyle: "expanded"}))
+        .on("error", swallowError)
+        .pipe(gulp.dest("app/static/")).pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(gulp.dest("app/static/"))
+        .pipe(browsersync.stream());
+}
+
+// Compile JS
+function js() {
+    return gulp
+        .src(["app/static/scripts/main.js"])
         .pipe(webpack({output: {filename: "bundle.js"}, mode: "production"}))
         .on("error", swallowError)
         .pipe(gulp.dest("app/static/"))
-})
+        .pipe(browsersync.stream());
+}
 
-gulp.task("sass", function () {
-    return gulp.src("app/static/styles/main.scss")
-        .pipe(sass({outputStyle: "compressed"}))
-        .on("error", swallowError)
-        .pipe(gulp.dest("app/static/"))
-})
+// Build AWS ZIP
+function build() {
+    del("dist");
 
-gulp.task("clean:tmp", function () {
-    return gulp.src(["tmp"], {read: false}).pipe(clean())
-})
+    gulp
+        .src("prod.env")
+        .pipe(rename(".env"))
+        .pipe(gulp.dest("tmp"));
+    gulp
+        .src(["*.py"])
+        .pipe(gulp.dest("tmp"));
+    gulp
+        .src(["requirements.txt"])
+        .pipe(gulp.dest("tmp"));
+    gulp
+        .src([".ebextensions/*"])
+        .pipe(gulp.dest("tmp/.ebextensions"));
+    gulp
+        .src(["app/**/*.py", "app/**/*.html", "app/**/*.txt"])
+        .pipe(gulp.dest("tmp/app"));
+    gulp
+        .src(["app/static/*.css", "app/static/*.js", "app/static/*.json"])
+        .pipe(gulp.dest("tmp/app/static"));
+    gulp
+        .src(["app/static/docs/**/*.pdf"])
+        .pipe(gulp.dest("tmp/app/static/docs"));
 
-gulp.task("compile", ["clean:tmp"], function () {
-    gulp.src(["dist"], {read: false}).pipe(clean())
-    gulp.src("prod.env").pipe(rename(".env")).pipe(gulp.dest("tmp"))
-    gulp.src(["*.py"]).pipe(gulp.dest("tmp"))
-    gulp.src(["requirements.txt"]).pipe(gulp.dest("tmp"))
-    gulp.src([".ebextensions/*"]).pipe(gulp.dest("tmp/.ebextensions"))
-    gulp.src(["app/**/*.py", "app/**/*.html", "app/**/*.txt"]).pipe(gulp.dest("tmp/app"))
-    gulp.src(["app/static/*.css", "app/static/*.js"]).pipe(gulp.dest("tmp/app/static"))
-    gulp.src(["app/static/docs/**/*.pdf"]).pipe(gulp.dest("tmp/app/static/docs"))
-    return gulp.src(["app/static/img/**/*.*"]).pipe(gulp.dest("tmp/app/static/img/"))
-})
+    return gulp
+        .src(["app/static/img/**/*.*"])
+        .pipe(gulp.dest("tmp/app/static/img/"));
+}
 
-gulp.task('archive', ["compile"], function () {
-    return gulp.src("tmp/**/*.*", {dot: true})
+function clean() {
+    return del("tmp");
+}
+
+function archive() {
+    return gulp
+        .src("tmp/**/*.*", {dot: true})
         .pipe(zip("dex-elb.zip"))
-        .pipe(gulp.dest("dist"))
-})
+        .pipe(gulp.dest("dist"));
+}
 
-gulp.task("default", function () {
-    gulp.start("sass")
-    gulp.start("js")
-    browserSync({notify: false, proxy: "127.0.0.1:5003"})
-    gulp.watch(["app/static/styles/**/*.scss"], ["sass"])
-    gulp.watch(["app/static/scripts/**/*.js"], ["js"])
-    gulp.watch(["app/templates/**/*.*", "app/static/styles/**/*.js", "app/static/styles/**/*.scss"], reload)
-})
+// Watch files
+function watchFiles() {
+  gulp.watch("app/static/styles/**/*", css);
+  gulp.watch("app/static/scripts/**/*", js);
+  gulp.watch("./app/templates/**/*", gulp.series(browserSyncReload));
+}
 
-gulp.task("build", function (callback) {
-    runSequence(
-        ["sass", "js"],
-        "archive",
-        "clean:tmp",
-        callback
-    )
-})
+const awsbuild = gulp.series(css, js, build, archive, clean);
+const watch = gulp.parallel(watchFiles, browserSync);
+
+exports.awsbuild = awsbuild;
+exports.default = watch;
